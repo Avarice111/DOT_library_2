20940c251b7b3f70db424cf555c3a529771591ed
Lalunaa <meeggi2@wp.pl>
2017-12-06 17:13:53 +0100

    IS5 Order model

---
+++ src/Dot.Library.Database/Model/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dot.Library.Database
+{
+    public class Order
+    {
+
+        public int OrderId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public User User { get; set; }
+
+        public List<Book> Books { get; set; }
+
+
+    }
+}
