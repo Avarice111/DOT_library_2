fc6a61a5af78b975a200289b4c5e49eb11a096d3
Kolejarzyk <pawel.dee@live.com>
2017-12-10 22:26:38 +0100

    IS11 CategoryControll fix

--- src/Dot.Library.Web/Controllers/CategoryController.cs
+++ src/Dot.Library.Web/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using Dot.Library.Database.Model;
+using System.Linq;
 
 namespace Dot.Library.Web.Controllers
 {
@@ -29,7 +30,7 @@ namespace Dot.Library.Web.Controllers
         [HttpGet]
         public IEnumerable<Category> GetAll() => _categories;
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetById")]
         public IActionResult GetById(long id)
         {
             var item = _categories.Find(t => t.ID == id);
@@ -48,8 +49,39 @@ namespace Dot.Library.Web.Controllers
                 return BadRequest();
             }
             _categories.Add(item);
-            return CreatedAtRoute(new {id = item.ID}, item);
+            return CreatedAtRoute("GetById",new {id = item.ID}, item);
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Category category)
+        {
+            if(category == null || category.ID != id )
+            {
+                return BadRequest();
+            }
+            var wantedUser = _categories.FirstOrDefault(x => x.ID == id);
+            if (category == null)
+            {
+                
+            }
+            _categories[wantedUser.ID] = wantedUser;
+            return new NoContentResult();
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var delete = _categories.FirstOrDefault(t => t.ID == id);
+            if(delete == null)
+            {
+                return NotFound();
+            }
+
+            _categories.RemoveAt(delete.ID);
+            return new NoContentResult();
+        }
+
       
     }
 }
